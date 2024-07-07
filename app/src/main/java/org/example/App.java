7c47e19d187a5ff929ddaabcb3fb428997783636
HKR414 <e1c21068@oit.ac.jp>
2024-07-07 14:13:03 +0900

    edited app file

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -11,8 +11,17 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
-        new Calculate().sumAB(2, 3);
-        new Calculate().sumA2B(1, 10);
-        new Calculate().sumOfoddAndevenA2B(1, 10);
+        float sumAB = new Calculate().sumAB(2, 3);
+        float aveAB = new Calculate().aveAB(sumAB);
+
+        float sumA2B = new Calculate().sumA2B(1, 10);
+        float aveA2B = new Calculate().aveA2B(sumA2B, 1, 10);
+
+        int odd = new Calculate().sumOfoddA2B(1, 10);
+        int even = new Calculate().sumOfevenA2B(1, 10);
+
+        System.out.println("Sum of 2 and 3 is " + sumAB + ". Average is " + aveAB + ".");
+        System.out.println("Sum of 1 to 10 is " + sumA2B + ". Average is " + aveA2B + ".");
+        System.out.println("Sum of odd of 1 to 10 is " + odd + ". Sum of even is " + even + ".");
     }
 }
--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -5,31 +5,47 @@ public class Calculate{
     return x + y;
   }
 
-  public void sumAB(int a, int b){
-    float sum=a+b;
-    float ave=sum/2;
-    System.out.println("Sum of " + a + " and " + b + " is " + sum + ". Average is " + ave + ".");
+  public float sumAB(int a, int b){
+    float sum = a + b;
+    return sum;
   }
 
-  public void sumA2B(int a, int b){
-    float sum=0;
-    float ave=0;
-    for(int i=a; i<=b; i++){
-      sum+=i;
+  public float aveAB(float sum){
+    float ave = sum / 2;
+    return ave;
+  }
+
+  public float sumA2B(int a, int b){
+    float sum = 0;
+    for(int i = a; i <= b; i++){
+      sum += i;
     }
-    ave=sum/(b-a+1);
-    System.out.println("Sum of " + a + " to " + b + " is " + sum + ". Average is " + ave + ".");
+    return sum;
   }
 
-  public void sumOfoddAndevenA2B(int a, int b){
-    int odd=0, even=0;
-    for(int i=a; i<=b; i++){
-      if(i%2==0){
-        even+=i;
-      }else{
-        odd+=i;
+  public float aveA2B(float sum, int a, int b){
+    int n = (b - a) + 1;
+    float ave = sum / n;
+    return ave;
+  }
+
+  public int sumOfoddA2B(int a, int b){
+    int sum = 0;
+    for(int i = a; i <= b; i++){
+      if(i % 2 != 0){
+        sum += i;
       }
     }
-    System.out.println("Sum of odd of " + a + " to " + b + " is " + odd + ". Sum of even is " + even + ".");
+    return sum;
+  }
+
+  public int sumOfevenA2B(int a, int b){
+    int sum = 0;
+    for(int i = a; i <= b; i++){
+      if(i % 2 == 0){
+        sum += i;
+      }
+    }
+    return sum;
   }
 }
